e4eb61b4c698e6bd0331a8407096cc6ea47b156b
Onan Thairu <thairuonan@gmail.com>
2022-01-29 13:13:36 +0300

    Step1 - Display feedback

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,13 +1,43 @@
 import React, { useState } from 'react'
 
+const Header = () => <div><h1>We would love to hear your feedback</h1></div>
+
+const Button = (props) => {
+  return (
+      <button className='btn' onClick={props.onclick}>{props.text}</button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <div>
+      <p>{props.text}: {props.count}</p>
+    </div>
+  )
+}
 const App = () => {
   // Save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const goodClicks = () => setGood(good + 1)
+
+  const neutralClicks = () => setNeutral(neutral + 1)
+
+  const badClicks = () => setBad(bad + 1)
+
   return (
-    <div></div>
+    <div>
+      <Header />
+      <Button text="Good" onclick={goodClicks} />
+      <Button text="Neutral" onclick={neutralClicks}/>
+      <Button text="Bad" onclick={badClicks}/>
+      <h2>Statistics</h2>
+      <Display text="Good" count={good} />
+      <Display text="Neutral" count={neutral} />
+      <Display text="Bad" count={bad} />
+    </div>
   )
 }
 
