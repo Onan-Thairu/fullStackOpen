f0485b60402f146484d61c19fbb702b053025b4a
Onan Thairu <thairuonan@gmail.com>
2022-02-15 20:18:38 +0300

    Step9 - Extend our app to allow an arbitrary no. of courses.

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,75 +1,87 @@
 import React from 'react'
 
 const Header = ({course}) => {
-  return (
-    <div>
-      <h1>{course.name}</h1>
-    </div>
-  )
+  return <h2>{course.name}</h2>
 }
 
-const Part = (props) => {
+
+const Part = ({parts}) => parts.map((part) => {
+  return <p key={part.id}> {part.name} - {part.exercises} </p>
+})
+
+
+const Content = ({courses}) => courses.map((course) => {
   return (
     <div>
-      <p>{props.part.name} - {props.part.exercises} </p>
+      <Header key={course.id} course={course}  />
+      <div>
+        <Part key={course.id} parts={course.parts} />
+      </div>
+      <Total course={course} />
     </div>
-  )
-}
-
-const Content = ({course}) => {
-  return course.parts.map((part) => <Part key={part.id} part={part} />)
-}
+    )
+})
 
 const Total = ({course}) => {
   const total = course.parts.reduce((sum, part) =>sum + part.exercises, 0)
   return (
     <div>
-      <p>Total of {total} exercises.</p>
+      <h4 key={course.id}>Total of {total} exercises.</h4>
     </div>
   )
 }
 
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+const Course = ({courses}) => <Content courses={courses} />
 
 const App = () => {
-  const course = {
-    id: 1,
+  const courses = [
+    {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-      id: 1
-    },
-    {
-      name:'Using props to pass data',
-      exercises: 7,
-      id: 2
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-      id: 3
-    },
-    {
-      name: 'Array mapping',
-      exercises: 13,
-      id:4
-    }
-  ]
-}
-
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name:'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id:4
+      }
+    ]  
+  },  
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      {
+        name: 'Routing',
+        exercises: 3,
+        id: 1
+      },
+      {
+        name: 'Middlewares',
+        exercises: 7,
+        id:2
+      }
+    ]
+  }
+]
   return (
     <div>
-      <Course course={course} />
+      <h1>Web Development Curriculum</h1>
+      <Course courses={courses} />
     </div>
   )
 }
