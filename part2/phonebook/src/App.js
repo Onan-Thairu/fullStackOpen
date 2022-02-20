2cdde488cda9a24e437a019ee59e661cee781699
Onan Thairu <thairuonan@gmail.com>
2022-02-20 15:58:04 +0300

    Step3 - Add phone number field and display.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,22 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name:'Arto Hellas'}
+    {
+      name:'Arto Hellas',
+      number:'+254759073578'
+    }
   ])
+
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   
   const addNew = ((event) => {
     event.preventDefault()
 
     const phoneObj = {
-      name:newName
+      name:newName,
+      number:newNumber
     }
     
     if(persons.some(person => person.name === newName)) {
@@ -19,24 +25,28 @@ const App = () => {
     } else {
       const allPersons = [...persons, phoneObj]
       setPersons(allPersons)
+      console.log(allPersons)
     }
     
   })
 
-  const handleInput = (event) => {
-    setNewName(event.target.value)
-  }
+  const handleName = (event) => setNewName(event.target.value)
 
-  const display = (persons) => persons.map((person) => <p key={person.name}>{person.name}</p>)
+  const handleNumber = (event) => setNewNumber(event.target.value)
+
+  const display = (persons) => persons.map((person) => {
+    return (
+      <p key={person.name}>{person.name}  {person.number}</p>
+      )
+  })
 
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNew}>
-        <div>
-          Name: <input onChange={handleInput} />
-        </div>
+        <div>Name:   <input onChange={handleName} /></div>
+        <div>Number: <input onChange={handleNumber} /> </div>
         <div>
           <button type='submit'>Add</button>
         </div>
