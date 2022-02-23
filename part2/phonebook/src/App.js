a8d44d53c0b58714f86a3bfeaf55d9052fa1554f
Onan Thairu <thairuonan@gmail.com>
2022-02-23 06:59:06 +0300

    Step6 - Modify phonebook. Initial state is fetched from the server using axios.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,19 +2,25 @@ import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name:'Arto Hellas', number:'+254759073578', id:1 },
-    { name:'Nick Kimani', number:'+254749302890', id:2 },
-    { name:'Mike Sothi', number:'+2547053490240', id:3 },
-    { name:'Mark Funyi', number:'+254734957208', id:4 },
-    { name:'Mark Kigwa', number:'+254729513014', id:5 },
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+
   const addNew = ((event) => {
     event.preventDefault()
 
