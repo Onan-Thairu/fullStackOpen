e37a99fa1426aba3fc6cb67d7f97a72f38d56023
Onan Thairu <thairuonan@gmail.com>
2022-02-26 14:35:54 +0300

    Step3 - Add openweathermap api and environment variables.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,26 +4,25 @@ import axios from 'axios'
 const App = () => {
   const [search, setSearch] = useState('')
   
-  const resultItems = document.getElementById('matchResults')
-
   useEffect(() => {
     axios
     .get('https://restcountries.com/v3.1/all')
     .then(response => {
       let data = response.data
       let searchResultsArray = []
+      const resultItems = document.getElementById('matchResults')
       
+      const weatherData = document.getElementById('weatherData')
 
       if(search){
         // Convert both country name and search field letters to lowercase and try to match.
         searchResultsArray = data.filter(country => country.name.common.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
 
-        let matchCountry = searchResultsArray[0]
-        let langObj = matchCountry.languages
-        let languagesArray = Object.values(langObj)
-
         
         if(searchResultsArray.length === 1) {
+          const matchCountry = searchResultsArray[0]
+          const langObj = matchCountry.languages
+          const languagesArray = Object.values(langObj)
           
           resultItems.innerHTML = `
           <h1> ${matchCountry.name.common}</h1>
@@ -33,20 +32,40 @@ const App = () => {
           <p>${languagesArray.map(lang => `<li>${lang}</li>`)}</p>
           <img src=${matchCountry.flags.svg} width=100 height=100 ></img>
           `
-          console.log(matchCountry)
+
+          axios
+            .get(`https://api.openweathermap.org/data/2.5/onecall?lat=${matchCountry.capitalInfo.latlng[0]}&lon=${matchCountry.capitalInfo.latlng[0]}&exclude={minutely,hourly,daily,alerts}&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}`)
+            .then(response => {
+              //console.log(response)
+              const temperature = response.data.current.temp
+              const windSpeed = response.data.current.wind_speed
+              const countryCapital = matchCountry.capital[0]
+
+              weatherData.innerHTML = `
+              <h2>Weather in ${countryCapital}</h2>
+              <p>Temperature: ${temperature}</p>
+              <img src='http://openweathermap.org/img/wn/02n@2x.png' >
+              <p>Wind: ${windSpeed} m/s</p>
+              `
+              
+            })
+
         } else if (searchResultsArray.length > 10) {
           resultItems.textContent = "Too many matches, specify another filter"
+          weatherData.textContent = ""
         } else if (searchResultsArray.length < 10) {
           resultItems.textContent = ""
-          console.log(searchResultsArray)
-          function display() {
-            alert("Button clicked.")
-          }
-          searchResultsArray.map(item => resultItems.innerHTML += `<p> ${item.name.common} <input type="button" value="Show" onclick="" />`)
+          weatherData.textContent = ""
+
+          //function display() {
+          //  alert("Button clicked.")
+          //}
+          searchResultsArray.map(item => resultItems.innerHTML += `<p> ${item.name.common} <input type="button" value="Show" />`)
         }
       }
     })
   })
+
 
   
   const handleSearch = (event => {
@@ -59,6 +78,7 @@ const App = () => {
         Find countries: <input type="search" onChange={handleSearch}  />
       </form>
       <p id="matchResults"></p>
+      <p id="weatherData"></p>
     </div>
   )
 }
